482657f70489c9aed26f73c5e314b094fe85f9d0
Dellshams <ayodelesaba@gmail.com>
2021-08-20 01:25:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const firstName = "Ayodele";
+const lastName = "Saba";
+const slackName = "Dellshams"
+
+let userDetails = `My name is ${firstName} ${lastName}, and my slack ID is ${slackName}.`
+
+console.log (userDetails);
+return userDetails;
